0a147ef805c3747f05a12869aa0bf7c739a0832e
Renan Fonseca <marcos.renan.cf@gmail.com>
2021-02-11 09:46:36 -0300

    connection db

---
+++ src/core/SingleConnection.java
@@ -0,0 +1,40 @@
+package core;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SingleConnection {
+	
+	
+	private static String db = "jdbc:postgresql://localhost:5432/login-system-java-jsp?autoReconnect=true";
+	private static String password = "admin";
+	private static String user = "postgres";
+	private static Connection connection = null;
+	
+	static {
+		connect();
+	}
+	
+	public SingleConnection() {
+		connect();
+	}
+	
+	private static void connect() {
+		try {
+			if(connection == null) {
+				Class.forName("org.postregsql.Driver");
+				connection = DriverManager.getConnection(db, user, password);
+				connection.setAutoCommit(false);
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("Erro ao conectar com o banco de dados");
+		}
+	}
+	
+	public static Connection getConnection() {
+		return connection;
+	}
+	
+	
+
+}
